feb75d2b3dbb74a89c1c58136b158881fb62bdd4
Jose Henrique <jh4832184@gmail.com>
2023-10-24 18:01:34 -0300

    view: add topics

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,34 @@
-'use client'
-import CardNew from "@/components/cardnew"
+'use client';
+import CardNew from "@/components/cardnew";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 export default function Home() {
   return (
     <main className='row container-fluid justify-content-around' style={{minHeight: "80vh"}}>
-      <h1>FastNews</h1>
+      <h1 className="mt-2">Brasil</h1>
       <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
       <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
       <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
       <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
       <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
       <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
+
+      <h1 className="mt-2">Futebol</h1>
+      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
+      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
+      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
+      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
+      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
+      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
+     
+      <h1 className="mt-2">Europa</h1>
+      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
+      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
+      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
+      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
+      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
+      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
+     
      </main>
   )
 }
