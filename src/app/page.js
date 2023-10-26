26515b4e6c21d3c4046d31306ef58d11e2d72290
Jose Henrique <jh4832184@gmail.com>
2023-10-25 21:15:40 -0300

    feat: simple request

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,16 @@ import CardNew from "@/components/cardnew";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
+
+
+  async function load () {
+    let apikey = "2a6927b988284b7b8f6466c6bc7f3176";
+    let search = "tesla"
+    let date = "2023-09-21"
+    let url = `https://newsapi.org/v2/everything?q=${search}&sortBy=publishedAt&apiKey=${apikey}`
+    fetch(url).then((response)=>response.json()).then((json)=>console.log(json))
+  }
+  load()
   return (
     <main className='row container-fluid justify-content-around' style={{minHeight: "80vh"}}>
       <h1 className="mt-2">Brasil</h1>
