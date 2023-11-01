914275fa1bf7584ca735c0870f912c03ce4ee4c5
Jose Henrique <jh4832184@gmail.com>
2023-10-31 22:23:41 -0300

    feat: request for others topics

--- src/app/page.js
+++ src/app/page.js
@@ -1,44 +1,64 @@
 'use client';
 import CardNew from "@/components/cardnew";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState, useEffect } from "react";
+
+import apikey from "./my_env";
+
 
 export default function Home() {
+  const [dataone, setDataOne] = useState([]);
+  const [datatwo, setDataTwo] = useState([]);
+  const [datathree, setDataThree] = useState([]); 
+  
+  async function load (search) {
+      let url = `https://newsapi.org/v2/everything?q=${search}&sortBy=publishedAt&apiKey=${apikey}`
+      let requisicao = await fetch(url)
+      .then((response)=> response.json())
+      .catch((error)=>{console.log("Erro na requisição")});
 
-
-  async function load () {
-    let apikey = "2a6927b988284b7b8f6466c6bc7f3176";
-    let search = "tesla"
-    let date = "2023-09-21"
-    let url = `https://newsapi.org/v2/everything?q=${search}&sortBy=publishedAt&apiKey=${apikey}`
-    fetch(url).then((response)=>response.json()).then((json)=>console.log(json))
+      let news = await requisicao.articles;
+      news = news.slice(0,9);
+      news = await news.map( (currentNew)=> {
+          return {"title":currentNew.title, "description":currentNew.description, "urlImg":currentNew.urlToImage, "urlSite":currentNew.url, "author":currentNew.author}
+      });
+      
+      if (search == "América do Sul") {
+        setDataOne(news); 
+      }
+      else if (search == "Futebol") {
+        setDataTwo(news)
+      }
+      else {
+        setDataThree(news)
+      }
+      
   }
-  load()
+
+  
+  useEffect(()=>{
+    load("América do Sul");
+    load("Futebol");
+    load("Europa");
+  }, [])
+  
+  
+  
   return (
     <main className='row container-fluid justify-content-around' style={{minHeight: "80vh"}}>
-      <h1 className="mt-2">Brasil</h1>
-      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
-      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
-      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
-      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
-      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
-      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
-
-      <h1 className="mt-2">Futebol</h1>
-      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
-      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
-      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
-      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
-      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
-      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
-     
+      <h1 className="mt-2">Brasil </h1>
+      {dataone && dataone.map((item,id)=>{
+        return <CardNew key={id} title={item.title} description={item.description} urlImg={item.urlImg} urlSite={item.urlSite} author={item.author} />
+      })}
+      
+      <h1 className="mt-2">Futebool</h1>
+      {datatwo[0] && datatwo.map((item,id)=>{
+        return <CardNew key={id} title={item.title} description={item.description} urlImg={item.urlImg} urlSite={item.urlSite} author={item.author} />
+      })}
       <h1 className="mt-2">Europa</h1>
-      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
-      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
-      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
-      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
-      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
-      <CardNew title="Flamengo rico?" description="O C.R.F divulgou sua receita anual chegando a UM BILHÃO! Isso mesmo torcedor, veja a seguir os principais." urlImg="images.jpeg" urlSite="https://react-bootstrap.github.io/" author="Renata Fan" />
-     
+      {datathree[0] && datathree.map((item,id)=>{
+        return <CardNew key={id} title={item.title} description={item.description} urlImg={item.urlImg} urlSite={item.urlSite} author={item.author} />
+      })}
      </main>
   )
 }
