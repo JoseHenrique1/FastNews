efd7c940223c5a394feed5ffa7f99ad41e26f3fc
Jose Henrique <jh4832184@gmail.com>
2023-11-05 13:36:01 -0300

    feat: add component fieldsearch and usestate

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,7 +1,32 @@
+'use client'
+import FiedSearch from "@/components/fieldsearch";
+import { useState } from "react";
 function Search() {
+    const [search, setSearch] = useState("");
+    
+
+    
     return ( 
-        <main>
-            search
+        <main style={{minHeight: "74.5vh"}}>
+            <div className="container-sm mt-3">
+                <FiedSearch search={search} setSearch={setSearch}/>
+            </div>
+
+            <div className="container-fluid bg-primary mt-3 mb-3 h-100" >
+                <h1>algo</h1>
+                <h1>algo</h1>
+                <h1>algo</h1>
+                <h1>algo</h1>
+                <h1>algo</h1>
+                <h1>algo</h1>
+                <h1>algo</h1>
+                <h1>algo</h1>
+                <h1>algo</h1>
+                <h1>algo</h1>
+                <h1>algo</h1>
+                <h1>algo</h1>
+            </div>
+            
         </main>
      );
 }
---
+++ src/components/fieldsearch/index.js
@@ -0,0 +1,22 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+
+function FiedSearch({search, setSearch}) {
+  return (
+      <InputGroup className="mb-3 ">
+        <Form.Control
+          placeholder=""
+          aria-label="Recipient's username"
+          aria-describedby="basic-addon2"
+          value={search}
+          onChange={(e)=>setSearch(e.target.value)}
+        />
+        <Button variant="outline-secondary" id="button-addon2">
+          Search
+        </Button>
+      </InputGroup>
+  );
+}
+
+export default FiedSearch;
