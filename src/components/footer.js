afbe02547d951be4a4d628c056e47a72fa808085
Jose Henrique <jh4832184@gmail.com>
2023-11-07 09:29:41 -0300

    view: add icons in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,19 @@
 "use client"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap-icons/font/bootstrap-icons.min.css"
 
 function Footer() {
     return ( 
         <footer className='container-fluid bg-dark text-white pt-1 pb-1'>
             <p className='text-center'>@FastNews</p>
-            <p className='text-center'>git hub, insta, email</p>
+            <div className='container-fluid d-flex justify-content-center'>
+                <div className='d-flex w-25 justify-content-around'>
+                    <i className="h2 bi bi-github"/>
+                    <i className="h2 bi bi-google"/>
+                    <i className="h2 bi bi-instagram"/>
+                </div>
+            </div>
+            
         </footer>
      );
 }
