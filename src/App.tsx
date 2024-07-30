e99b730c6407f87f47c7c86267be02b2c5b86564
Jose Henrique <jh4832184@gmail.com>
2024-07-30 18:51:37 -0300

    view: add main component and style link

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { Footer } from "./components/footer"
 import { Header } from "./components/header"
+import { Main } from "./components/main"
 import { Navbar } from "./components/navbar"
 
 function App() {
   return (
     <div>
-    <Header/>
-    <Navbar/>
+      <Header />
+      <Navbar />
+      <Main />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/main.tsx
@@ -0,0 +1,35 @@
+import { CardNews } from "./card-news";
+export function Main() {
+  return (
+    <main className="bg-gray-50 p-4 gap-4 grid grid-cols-12">
+
+      <div className="col-span-12 space-y-4 lg:col-span-8">
+        <div className="h-20 flex flex-col justify-center ">
+          <h1 className="text-2xl font-medium">Trending news</h1>
+          <h2>Check out the main news from around the world</h2>
+        </div>
+        <div className="space-y-4">
+          <CardNews />
+          <CardNews />
+          <CardNews />
+        </div>
+      </div>
+
+
+      <div className="col-span-12 space-y-4 lg:col-span-4">
+        <div className="h-20 flex flex-col justify-center ">
+          <h1 className="text-2xl font-medium">Champions League</h1>
+          <h2>The best football league in the world</h2>
+        </div>
+        <div className="bg-cyan-400 w-60 h-40">
+          CARROUSEL
+        </div>
+        <div className="border-y-2 border-cyan-400 p-4">
+          <h1>About</h1>
+          <p>Web site of current news and ...</p>
+        </div>
+      </div>
+
+    </main>
+  );
+}
