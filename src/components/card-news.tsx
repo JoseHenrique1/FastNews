1c6c367ff6c283708df29e4947015bdd824d6998
Jose Henrique <jh4832184@gmail.com>
2024-07-30 18:50:50 -0300

    view: add card-news

---
+++ src/components/card-news.tsx
@@ -0,0 +1,14 @@
+import photo from "../assets/img/usina.jpg"
+export function CardNews() {
+  return (
+    <div className="bg-cyan-400 rounded flex flex-col max-w-screen-lg p-4 gap-1 sm:h-40 sm:gap-4 sm:flex-row">
+      <img
+        src={photo}
+        alt=""
+        className="rounded" />
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque omnis alias sint unde asperiores maiores, incidunt reiciendis harum consectetur possimus porro quisquam commodi laudantium architecto doloremque, obcaecati repellendus! Rerum, nobis!
+      </div>
+    </div>
+  );
+}
