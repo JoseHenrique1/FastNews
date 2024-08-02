09dfa56d153aaaa1634d7da7364311da93b0d16c
Jose Henrique <jh4832184@gmail.com>
2024-08-02 10:40:47 -0300

    view: add social networks

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,14 +1,19 @@
-import { Facebook, Github, Instagram, Youtube } from "lucide-react";
+import { Github, Instagram, Linkedin } from "lucide-react";
 
 export function Footer() {
-  return ( 
-  <footer className="bg-blue-800 p-4 space-y-2">
-    <h1 className="w-full text-center text-2xl font-medium text-gray-300">Fastnews</h1>
-    <div className="flex justify-center gap-4">
-      <Github color="#d1d5db"/>
-      <Instagram color="#d1d5db"/>
-      <Facebook color="#d1d5db"/>
-      <Youtube color="#d1d5db"/>
-    </div>
-  </footer> );
+  return (
+    <footer className="bg-blue-800 p-4 space-y-2">
+      <h1 className="w-full text-center text-2xl font-medium text-gray-300">Fastnews</h1>
+      <div className="flex justify-center gap-4">
+        <a href="https://github.com/JoseHenrique1" target="_blank">
+          <Github color="#d1d5db" />
+        </a>
+        <a href="https://www.linkedin.com/in/jos%C3%A9-henrique-araujo-da-silva-43970a274/" target="_blank">
+          <Linkedin color="#d1d5db" />
+        </a>
+        <a href="https://www.instagram.com/josehenrii10/" target="_blank">
+          <Instagram color="#d1d5db" />
+        </a>
+      </div>
+    </footer>);
 }
